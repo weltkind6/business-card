7da4e03b0f78587ec5b654a82b22ea1384fd8e13
yaroslavzefi <yaroslavzef@gmail.com>
2023-11-22 21:39:13 +0300

    test

--- src/AppRoutes/AppRoutes.js
+++ src/AppRoutes/AppRoutes.js
@@ -5,35 +5,42 @@ import {getApiUrl, getHash} from "../api/api";
 import NotFound from "../components/pages/NotFound/NotFound";
 import {useEffect} from "react";
 import axios from "axios";
+import TestPage from "../components/TestPage/TestPage";
 
 const AppRoutes = () => {
-    const location = useLocation();
-    const navigate = useNavigate()
-
-    const hash = getHash()
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await axios.get(getApiUrl())
-            } catch (error) {
-                navigate("/")
-                console.log(error)
-            }
-        }
-        fetchData().catch(error => console.log(error))
-    }, [])
-
-    if (location.pathname === "/show" && !hash) {
-        return <Navigate to="/" replace />;
-    }
+    // const location = useLocation();
+    // const navigate = useNavigate()
+    //
+    // const hash = getHash()
+    //
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             await axios.get(getApiUrl())
+    //         } catch (error) {
+    //             navigate("/")
+    //             console.log(error)
+    //         }
+    //     }
+    //     fetchData().catch(error => console.log(error))
+    // }, [])
+    //
+    // if (location.pathname === "/show" && !hash) {
+    //     return <Navigate to="/" replace />;
+    // }
 
     return (
         <Routes>
-            <Route exact path="/" element={<MainPage/>}/>
-            <Route exact path={"show/" + hash} element={<UserPage/>}/>
-            <Route exact path="/404" element={<NotFound/>}/>
-            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/test" element={<TestPage />}/>
+            {/*<Route exact path="/" element={<MainPage/>}/>*/}
+            {/*<Route*/}
+            {/*    exact*/}
+            {/*    path="/show/:hash"*/}
+            {/*    element={<UserPage />}*/}
+            {/*/>*/}
+
+            {/*<Route exact path="/404" element={<NotFound/>}/>*/}
+            {/*<Route path="*" element={<Navigate to="/" replace />} />*/}
         </Routes>
     );
 };
