8d3a77f232aa698dc4d5f2edc4fee2be40e236a4
yaroslavzefi <yaroslavzef@gmail.com>
2023-11-22 20:10:00 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from "./service-worker";
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-serviceWorker.register();
+
 root.render(
   <React.StrictMode>
     <App />
