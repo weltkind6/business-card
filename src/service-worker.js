d35729cb0ef6f0f59404a1d09cfef9050b17c08d
yaroslavzefi <yaroslavzef@gmail.com>
2023-11-22 18:54:22 +0300

    PWA

---
+++ src/service-worker.js
@@ -0,0 +1,35 @@
+window.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/icon.png',
+                // Добавьте другие ресурсы вашего приложения, которые нужно закэшировать
+            ]);
+        })
+    );
+});
+
+window.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+export function register() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then(registration => {
+                    console.log('Service Worker registered:', registration);
+                })
+                .catch(error => {
+                    console.log('Service Worker registration failed:', error);
+                });
+        });
+    }
+}
