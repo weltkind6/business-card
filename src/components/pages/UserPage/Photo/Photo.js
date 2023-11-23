fd7539146f73673df79a4a308e120ae47d0c55d8
yaroslavzefi <yaroslavzef@gmail.com>
2023-11-23 11:19:01 +0300

    null vs photo

--- src/components/pages/UserPage/Photo/Photo.js
+++ src/components/pages/UserPage/Photo/Photo.js
@@ -1,9 +1,12 @@
+import noAvatar from '../../../../img/no-avatar.png'
 import './style.css';
 
 const Photo = ({photo}) => {
     return (
         <div className="photo">
-            <img src={photo} alt="foto"/>
+            <img
+                src={photo === null ? noAvatar : photo} alt="foto"
+            />
         </div>
     );
 };
